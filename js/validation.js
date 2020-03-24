2698c5e5ffa4cd6c57cde3f8fc7b9c3479567c19
Tanya Hristova <tanche98@gmail.com>
2020-03-25 00:32:55 +0200

    replace document.write function with DOM manipulation functions and add html validation of data

--- js/validation.js
+++ js/validation.js
@@ -1,9 +1,5 @@
 function validate()
 {
-	//document.body.appendChild(form);
-	
-	console.log("eho");
-	
 	let username = document.getElementById("username").value;
 	let usernamePattern = /^[a-zA-Z0-9_]{3,10}$/;
 	
@@ -14,23 +10,23 @@ function validate()
 	
 	if (! validateField(username, usernamePattern))
 	{
-		document.write("<div>Username " + username + " not correct. ");
-		document.write("Username should be between 3 and 10 symbols containing letters, digits and _</div>");
-		document.write("<a href='./js-validated-form.html'>Go back to the form.</a>");
+		document.getElementById('not-successful').innerHTML = "Username " + username + " not correct." + 
+		"Username should be between 3 and 10 symbols containing letters, digits and _");
+		document.getElementById('redirection').innerHTML = "Go back to the form.");
 		return;
 	}
 	
 	if (! validateField(pass, passPattern))
 	{
-		document.write("<p>Password should be at least 6 symbols containing at least 1 capital letter, 1 small letter and 1 digit!</p>");
-		document.write("<a href='./js-validated-form.html'>Go back to the form.</a>");
+		document.getElementById('not-successful').innerHTML = "Password should be at least 6 symbols containing at least 1 capital letter, 1 small letter and 1 digit!" + 
+		document.getElementById('redirection').innerHTML = "Go back to the form.");
 		return;
 	}
 	
 	if (pass !== pass2)
 	{
-		document.write("<p>Reentered password should be the same as the first one!</p>");
-		document.write("<a href='./js-validated-form.html'>Go back to the form.</a>");
+		document.getElementById('not-successful').innerHTML = "Reentered password should be the same as the first one!";
+		document.getElementById('redirection').innerHTML = "Go back to the form.");
 		return;
 	}
 }
