288c1ef4694349ddccc1bf64a198fce0245ba7d5
Tanya Hristova <tanche98@gmail.com>
2020-04-13 14:35:12 +0300

    add field validation function

--- sql/validation.php
+++ sql/validation.php
@@ -2,45 +2,36 @@
 
 include "./db-connection.php";
 
-$logfile = fopen("logfile.txt", "w");
-
-$valid = array();
-$errors = array();
-
 define("MAX_LENGTH_SUBJECT", 128);
 define("MAX_LENGTH_LECTURER", 128);
 define("MAX_LENGTH_DESCR", 1024);
 
+function validate(String $key, int $maxLength, String $field, &$valid, &$errors) {
+
+	if(!$field) {
+		$errors[$key] = "Полето е задължително.";
+	} elseif (strlen($field) > $maxLength) {
+		$errors[$key] = "Полето има максимална дължина: " . $maxLength . " символа.";
+	} else {
+		$valid[$key] = $field;
+	}
+}
+
+$valid = array();
+$errors = array();
+
+$logfile = fopen("logfile.txt", "w");
+
 if ($_POST) {
-$title = $_POST["title"];
-$lecturer = $_POST["lecturer"];
-$description = $_POST["description"];
+	$title = $_POST["title"];
+	$lecturer = $_POST["lecturer"];
+	$description = $_POST["description"];
 
-if (!$title) {
-$errors["title"] = "Име на предмета е задължително поле.";
-} elseif (strlen($title) > MAX_LENGTH_SUBJECT) {
-$errors["title"] = "Името на предмета има максимална дължина " . MAX_LENGTH_SUBJECT . " символа.";
-} else {
-$valid["title"] = $title;
-}
+	validate("title", MAX_LENGTH_SUBJECT, $title, $valid, $errors);
+	validate("lecturer", MAX_LENGTH_LECTURER, $lecturer, $valid, $errors);
+	validate("description", MAX_LENGTH_DESCR, $description, $valid, $errors);
 
-if (!$lecturer) {
-$errors["lecturer"] = "Преподавател е задължително поле.";
-} elseif (strlen($lecturer) > MAX_LENGTH_LECTURER) {
-$errors["lecturer"] = "Преподавател има максимална дължина " . MAX_LENGTH_LECTURER . " символа.";
-} else {
-$valid["lecturer"] = $lecturer;
-}
-
-if (!$description) {
-$errors["description"] = "Описание е задължително поле.";
-} elseif (strlen($description) > MAX_LENGTH_DESCR) {
-$errors["description"] = "Описание има максимална дължина " . MAX_LENGTH_DESCR . " символа.";
-} else {
-$valid["description"] = $description;
-}
-
-if(count($valid) == 3)
+if(count($errors) == 0)
 {
 	fwrite($logfile, "Име на предмет: " . $title . " \n");
 	fwrite($logfile, "Име на преподавател: " . $lecturer . " \n");
